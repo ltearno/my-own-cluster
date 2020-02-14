9a1687cda88981e103f5f6576862d81bcf3614e4
Arnaud Tournier <ltearno@gmail.com>
2020-02-14 08:07:39 +0100

    rust demo wip

--- samples/rust-nowasi/src/lib.rs
+++ samples/rust-nowasi/src/lib.rs
@@ -1,11 +1,8 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+mod moc;
+use moc::*;
 
 #[derive(Serialize, Deserialize)]
 struct WatchdogServiceStatus {
@@ -42,88 +39,6 @@ fn message_response(message: &str) {
     write_buffer(get_output_buffer_id(), serialized.as_bytes());
 }
 
-// import the 'my-own-cluster' library
-mod raw {
-    #[link(wasm_import_module = "my-own-cluster")]
-    extern {
-        pub fn test() -> u32;
-        pub fn print_debug(buffer: *const u8, length: u32) -> u32;
-        pub fn register_buffer(buffer: *const u8, length: u32) -> u32;
-        pub fn get_buffer_size(buffer_id: u32) -> u32;
-        pub fn get_buffer(buffer_id: u32, buffer: *const u8, length: u32) -> u32;
-        pub fn write_buffer(buffer_id: u32, buffer: *const u8, length: u32) -> u32;
-        pub fn write_buffer_header(buffer_id: u32, name: *const u8, name_length: u32, value: *const u8, value_length: u32) -> u32;
-        pub fn free_buffer(buffer_id: u32) -> i32;
-        pub fn get_input_buffer_id() -> u32;
-        pub fn get_output_buffer_id() -> u32;
-        pub fn get_url(buffer: *const u8, length: u32) -> u32;
-    }
-}
-
-pub fn get_input_buffer_id() -> u32 {
-    unsafe {
-        raw::get_input_buffer_id()
-    }
-}
-
-pub fn get_output_buffer_id() -> u32 {
-    unsafe {
-        raw::get_output_buffer_id()
-    }
-}
-
-pub fn print_debug(s: &str) {
-    unsafe {
-        let bytes = s.as_bytes();
-        
-        unsafe {
-            raw::print_debug(bytes.as_ptr(), bytes.len() as u32);
-        }
-    }
-}
-
-pub fn read_buffer(buffer_id: u32) -> Vec<u8> {
-    unsafe {
-        let buffer_size = raw::get_buffer_size(buffer_id);
-        let mut dest = Vec::with_capacity(buffer_size as usize);
-        dest.resize(buffer_size as usize, 0);
-        raw::get_buffer(buffer_id, dest.as_ptr(), buffer_size);
-        dest
-    }
-}
-
-pub fn read_buffer_as_string(buffer_id: u32) -> String {
-    String::from_utf8(read_buffer(buffer_id)).unwrap()
-}
-
-pub fn get_url(url: &str) -> String {
-    unsafe {
-        let bytes = url.as_bytes();
-        let buffer_id = raw::get_url(bytes.as_ptr(), bytes.len() as u32);
-        let content = read_buffer_as_string(buffer_id);
-        raw::free_buffer(buffer_id);
-
-        content
-    }
-}
-
-pub fn write_buffer(buffer_id: u32, buffer: &[u8]) -> u32 {
-    unsafe {
-        raw::write_buffer(buffer_id, buffer.as_ptr(), buffer.len() as u32)
-    }
-}
-
-pub fn write_buffer_header(buffer_id: u32, name: &str, value: &str) {
-    unsafe {
-        raw::write_buffer_header(
-            buffer_id, 
-            name.as_bytes().as_ptr(), 
-            name.as_bytes().len() as u32, 
-            value.as_bytes().as_ptr(), 
-            value.as_bytes().len() as u32);
-    }
-}
-
 #[no_mangle]
 pub extern fn getStatus() -> u32 {
     let status = &WatchdogStatus{
@@ -131,18 +46,12 @@ pub extern fn getStatus() -> u32 {
         services: HashMap::new(),
     };
 
-    match serde_json::to_string(&status) {
-        Ok(serialized) => {
-            write_buffer_header(get_output_buffer_id(), "content-type", "application/json");
-            write_buffer(get_output_buffer_id(), serialized.as_bytes());
-            
-            serialized.as_bytes().len() as u32
-        },
-        Err(err) => {
-            print_debug(&format!("error when serializing our response : {}", err));
-            13 as u32
-        },
-    }
+    let serialized = serde_json::to_string(&status).unwrap();
+
+    write_buffer_header(get_output_buffer_id(), "content-type", "application/json");
+    write_buffer(get_output_buffer_id(), serialized.as_bytes());
+    
+    200
 }
 
 #[no_mangle]
@@ -154,7 +63,7 @@ pub extern fn postStatus() -> u32 {
             // TODO
             // get timestamp
             // set key value for service in serverless database
-            message_response("status saved, thanks");
+            message_response(&format!("status for '{}' saved, thanks", status.name));
             200 as u32
         },
         Err(err) =>{
---
+++ samples/rust-nowasi/src/moc.rs
@@ -0,0 +1,87 @@
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// import the 'my-own-cluster' library
+pub mod raw {
+    #[link(wasm_import_module = "my-own-cluster")]
+    extern {
+        pub fn test() -> u32;
+        pub fn print_debug(buffer: *const u8, length: u32) -> u32;
+        pub fn register_buffer(buffer: *const u8, length: u32) -> u32;
+        pub fn get_buffer_size(buffer_id: u32) -> u32;
+        pub fn get_buffer(buffer_id: u32, buffer: *const u8, length: u32) -> u32;
+        pub fn write_buffer(buffer_id: u32, buffer: *const u8, length: u32) -> u32;
+        pub fn write_buffer_header(buffer_id: u32, name: *const u8, name_length: u32, value: *const u8, value_length: u32) -> u32;
+        pub fn free_buffer(buffer_id: u32) -> i32;
+        pub fn get_input_buffer_id() -> u32;
+        pub fn get_output_buffer_id() -> u32;
+        pub fn get_url(buffer: *const u8, length: u32) -> u32;
+    }
+}
+
+pub fn get_input_buffer_id() -> u32 {
+    unsafe {
+        raw::get_input_buffer_id()
+    }
+}
+
+pub fn get_output_buffer_id() -> u32 {
+    unsafe {
+        raw::get_output_buffer_id()
+    }
+}
+
+pub fn print_debug(s: &str) {
+    unsafe {
+        let bytes = s.as_bytes();
+        
+        unsafe {
+            raw::print_debug(bytes.as_ptr(), bytes.len() as u32);
+        }
+    }
+}
+
+pub fn read_buffer(buffer_id: u32) -> Vec<u8> {
+    unsafe {
+        let buffer_size = raw::get_buffer_size(buffer_id);
+        let mut dest = Vec::with_capacity(buffer_size as usize);
+        dest.resize(buffer_size as usize, 0);
+        raw::get_buffer(buffer_id, dest.as_ptr(), buffer_size);
+        dest
+    }
+}
+
+pub fn read_buffer_as_string(buffer_id: u32) -> String {
+    String::from_utf8(read_buffer(buffer_id)).unwrap()
+}
+
+pub fn get_url(url: &str) -> String {
+    unsafe {
+        let bytes = url.as_bytes();
+        let buffer_id = raw::get_url(bytes.as_ptr(), bytes.len() as u32);
+        let content = read_buffer_as_string(buffer_id);
+        raw::free_buffer(buffer_id);
+
+        content
+    }
+}
+
+pub fn write_buffer(buffer_id: u32, buffer: &[u8]) -> u32 {
+    unsafe {
+        raw::write_buffer(buffer_id, buffer.as_ptr(), buffer.len() as u32)
+    }
+}
+
+pub fn write_buffer_header(buffer_id: u32, name: &str, value: &str) {
+    unsafe {
+        raw::write_buffer_header(
+            buffer_id, 
+            name.as_bytes().as_ptr(), 
+            name.as_bytes().len() as u32, 
+            value.as_bytes().as_ptr(), 
+            value.as_bytes().len() as u32);
+    }
+}
