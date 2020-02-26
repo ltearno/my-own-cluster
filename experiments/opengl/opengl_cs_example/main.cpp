56b24a74367428c25da75342e306e6a9fb13af25
Arnaud Tournier <ltearno@gmail.com>
2020-02-26 11:44:45 +0100

    wip

--- experiments/opengl/opengl_cs_example/main.cpp
+++ experiments/opengl/opengl_cs_example/main.cpp
@@ -1,6 +1,6 @@
 #include "opengl.h"
 
-GLuint renderHandle, computeHandle;
+GLuint computeHandle;
 
 void updateTex(int);
 void draw();
@@ -9,14 +9,12 @@ int main() {
 	initGL();
 
 	GLuint texHandle = genTexture();
-	renderHandle = genRenderProg(texHandle);
 	computeHandle = genComputeProg(texHandle);
 
 	printf("ok 2\n");
 
 	for (int i = 0; i < 1024; ++i) {
 		updateTex(i);
-		draw();
 	}
 
 	return 0;
@@ -25,16 +23,6 @@ int main() {
 void updateTex(int frame) {
 	glUseProgram(computeHandle);
 	glUniform1f(glGetUniformLocation(computeHandle, "roll"), (float)frame*0.01f);
-	//glDispatchCompute(512/16, 512/16, 1); // 512^2 threads in blocks of 16^2
-	printf("before dispatch\n");
-	glDispatchCompute(1, 1, 1);
-	printf("after dispatch\n");
+	glDispatchCompute(512/16, 512/16, 1); // 512^2 threads in blocks of 16^2
 	checkErrors("Dispatch compute shader");
-}
-
-void draw() {
-	glUseProgram(renderHandle);
-	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-	swapBuffers();
-	checkErrors("Draw screen");
 }
