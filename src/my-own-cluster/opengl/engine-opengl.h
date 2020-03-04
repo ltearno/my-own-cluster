f097b4d6be62d901b29a514bc52f7609e926b7e7
Arnaud Tournier <ltearno@gmail.com>
2020-03-04 09:40:56 +0100

    reorg c files

---
+++ src/my-own-cluster/opengl/engine-opengl.h
@@ -0,0 +1,40 @@
+#define GL_GLEXT_PROTOTYPES
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+#include <gbm.h>
+
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+
+#include <GL/gl.h>
+#include <GL/glext.h>
+
+typedef struct {
+  int renderDeviceFd;
+  struct gbm_device *gbm;
+   EGLDisplay egl_dpy;
+   EGLConfig config;
+} DeviceInformation;
+
+typedef struct {
+   EGLContext core_ctx;
+} ContextInformation;
+
+int initOpenGLDevice(DeviceInformation *info);
+int initOpenGLContext(DeviceInformation *dInfo, ContextInformation *info);
+int setContext(DeviceInformation *dInfo, ContextInformation *info);
+int clearContext(DeviceInformation *dInfo, ContextInformation *info);
+int compileAndBindShader(const char *shader_source);
+
+int bindStorageBuffer(int binding, void* buffer, int size);
+int bindTexture2DRGBAFloat(int binding, int textureWidth, int textureHeight);
+int getStorageBuffer(int bufferIndex, void* buffer, int size);
+int getTextureBuffer(int textureIndex, void* buffer, int size);
+int deleteBuffer(int bufferIndex);
+int deleteTexture(int textureIndex);
