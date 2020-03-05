58c45cb7842e32fed6bf6e7725e32fb28eb4a4af
Arnaud Tournier <ltearno@gmail.com>
2020-03-05 11:28:56 +0100

    refactor

--- src/my-own-cluster/enginewasm/engine-wasm.go
+++ src/my-own-cluster/enginewasm/engine-wasm.go
@@ -12,6 +12,33 @@ import (
 	wasm3 "github.com/ltearno/go-wasm3"
 )
 
+type MyOwnClusterWASMAPIPlugin struct{}
+
+func NewMyOwnClusterWASMAPIPlugin() WASMAPIPlugin {
+	return &MyOwnClusterWASMAPIPlugin{}
+}
+
+func (p *MyOwnClusterWASMAPIPlugin) Bind(wctx *WasmProcessContext) {
+	importedModules := wctx.GetImportedModules()
+	if _, ok := importedModules["my-own-cluster"]; !ok {
+		return
+	}
+
+	if wctx.Fctx.Trace {
+		fmt.Println("binding wasm api providers...")
+	}
+
+	for module, _ := range importedModules {
+		apiProvider := wctx.Fctx.Orchestrator.GetAPIProvider(module)
+		if apiProvider == nil {
+			fmt.Printf("error : cannot bind module '%s' because api provider is not found. It might be found in stored functions though...\n", module)
+			continue
+		}
+
+		apiProvider.BindToExecutionEngineContext(wctx)
+	}
+}
+
 // WasmProcessContext represents a running WASM context
 type WasmProcessContext struct {
 	Fctx *common.FunctionExecutionContext
