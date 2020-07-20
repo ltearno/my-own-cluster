f894d2fc72c9da7c435ca4c0c4e19416bc8eedf0
Arnaud Tournier <ltearno@gmail.com>
2020-07-20 15:25:55 +0200

    wip

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -9,5 +9,6 @@ go get github.com/syndtr/goleveldb/leveldb
 go get github.com/ltearno/go-wasm3
 go get gopkg.in/ltearno/go-duktape.v3
 go get golang.org/x/sys/unix
+go get github.com/gorilla/websocket
 
 exit 0
