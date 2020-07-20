5f8b4d90c84372eb9e1a2d099e9cc257decdad2f
Arnaud Tournier <ltearno@gmail.com>
2020-07-20 16:26:25 +0200

    clean

---
+++ src/my-own-cluster/common/echangebuffer-inmemory.go
@@ -0,0 +1,63 @@
+package common
+
+/*
+	In memory implementation of ExchangeBuffer
+*/
+type InMemoryExchangeBuffer struct {
+	headers map[string]string
+	buffer  []byte
+}
+
+func (p *InMemoryExchangeBuffer) GetHeader(name string) (string, bool) {
+	s, ok := p.headers[name]
+	return s, ok
+}
+
+func (p *InMemoryExchangeBuffer) SetHeader(name string, value string) {
+	p.headers[name] = value
+}
+
+func (p *InMemoryExchangeBuffer) GetHeadersCount() int {
+	return len(p.headers)
+}
+
+func (p *InMemoryExchangeBuffer) GetHeaders(cb func(name string, value string)) {
+	for name, value := range p.headers {
+		cb(name, value)
+	}
+}
+
+func (p *InMemoryExchangeBuffer) GetBuffer() []byte {
+	return p.buffer
+}
+
+func (p *InMemoryExchangeBuffer) Read(buffer []byte) int {
+	return 0
+}
+
+func (p *InMemoryExchangeBuffer) Write(buffer []byte) (int, error) {
+	// TODO WARNING THIS DOES NOT TAKE WRITE POS IN ACCOUNT !!!
+	p.buffer = appendSlice(p.buffer, buffer)
+
+	return len(buffer), nil
+}
+
+/*
+	TODO : should be called 'Release' and trigger the exchange buffer GC
+*/
+func (p *InMemoryExchangeBuffer) Close() int {
+	return 0
+}
+
+func appendSlice(slice []byte, data []byte) []byte {
+	m := len(slice)
+	n := m + len(data)
+	if n > cap(slice) {
+		newSlice := make([]byte, (n+1)*2)
+		copy(newSlice, slice)
+		slice = newSlice
+	}
+	slice = slice[0:n]
+	copy(slice[m:n], data)
+	return slice
+}
--- src/my-own-cluster/common/exchangebuffer.go
+++ src/my-own-cluster/common/exchangebuffer.go
@@ -1,9 +1,5 @@
 package common
 
-import (
-	"sync"
-)
-
 /*
 	ExchangeBuffer is a byte buffer together with a set of headers.
 
@@ -24,32 +20,27 @@ type ExchangeBuffer interface {
 	Close() int
 }
 
-/*
-	In memory implementation of ExchangeBuffer
-*/
-type InMemoryExchangeBuffer struct {
-	headers map[string]string
-	buffer  []byte
-}
-
 func (o *Orchestrator) CreateExchangeBuffer() int {
-	var lock sync.Mutex
-
-	lock.Lock()
+	o.lock.Lock()
 	bufferID := o.nextExchangeBufferID
 	o.nextExchangeBufferID++
 	o.exchangeBuffers[int(bufferID)] = NewMemoryExchangeBuffer()
-	lock.Unlock()
+	o.lock.Unlock()
 
 	return int(bufferID)
 }
 
 func (o *Orchestrator) GetExchangeBuffer(bufferID int) ExchangeBuffer {
-	return o.exchangeBuffers[bufferID]
+	o.lock.Lock()
+	buffer := o.exchangeBuffers[bufferID]
+	o.lock.Unlock()
+	return buffer
 }
 
 func (o *Orchestrator) ReleaseExchangeBuffer(bufferID int) {
+	o.lock.Lock()
 	delete(o.exchangeBuffers, bufferID)
+	o.lock.Unlock()
 }
 
 func NewMemoryExchangeBuffer() *InMemoryExchangeBuffer {
@@ -57,58 +48,4 @@ func NewMemoryExchangeBuffer() *InMemoryExchangeBuffer {
 		headers: make(map[string]string),
 		buffer:  []byte{},
 	}
-}
-
-func (p *InMemoryExchangeBuffer) GetHeader(name string) (string, bool) {
-	s, ok := p.headers[name]
-	return s, ok
-}
-
-func (p *InMemoryExchangeBuffer) SetHeader(name string, value string) {
-	p.headers[name] = value
-}
-
-func (p *InMemoryExchangeBuffer) GetHeadersCount() int {
-	return len(p.headers)
-}
-
-func (p *InMemoryExchangeBuffer) GetHeaders(cb func(name string, value string)) {
-	for name, value := range p.headers {
-		cb(name, value)
-	}
-}
-
-func (p *InMemoryExchangeBuffer) GetBuffer() []byte {
-	return p.buffer
-}
-
-func (p *InMemoryExchangeBuffer) Read(buffer []byte) int {
-	return 0
-}
-
-func (p *InMemoryExchangeBuffer) Write(buffer []byte) (int, error) {
-	// TODO WARNING THIS DOES NOT TAKE WRITE POS IN ACCOUNT !!!
-	p.buffer = appendSlice(p.buffer, buffer)
-
-	return len(buffer), nil
-}
-
-/*
-	TODO : should be called 'Release' and trigger the exchange buffer GC
-*/
-func (p *InMemoryExchangeBuffer) Close() int {
-	return 0
-}
-
-func appendSlice(slice []byte, data []byte) []byte {
-	m := len(slice)
-	n := m + len(data)
-	if n > cap(slice) {
-		newSlice := make([]byte, (n+1)*2)
-		copy(newSlice, slice)
-		slice = newSlice
-	}
-	slice = slice[0:n]
-	copy(slice[m:n], data)
-	return slice
 }
