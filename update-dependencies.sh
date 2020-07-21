e74c0d1880e0cd9e19cba334176718224395148c
Arnaud Tournier <ltearno@gmail.com>
2020-07-21 15:01:28 +0200

    wip

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -9,6 +9,7 @@ go get github.com/ltearno/go-wasm3
 go get gopkg.in/ltearno/go-duktape.v3
 go get github.com/gorilla/websocket
 go get github.com/golang-collections/go-datastructures/queue
-go get -u golang.org/x/sys
+#go get -u golang.org/x/sys
+git clone https://github.com/golang/sys.git --depth=1 src/golang.org/x/sys
 
 exit 0
