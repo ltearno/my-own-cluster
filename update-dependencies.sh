a669f02845d78be1d2d4b93b6c0cd363fd263165
Arnaud Tournier <ltearno@gmail.com>
2020-07-21 14:30:44 +0200

    test

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -3,11 +3,11 @@ export GOPATH=$(pwd)
 
 go env
 
+go get golang.org/x/sys/unix
 go get -u github.com/jteeuwen/go-bindata/...
 go get github.com/syndtr/goleveldb/leveldb
 go get github.com/ltearno/go-wasm3
 go get gopkg.in/ltearno/go-duktape.v3
-go get golang.org/x/sys/unix
 go get github.com/gorilla/websocket
 go get github.com/golang-collections/go-datastructures/queue
 
